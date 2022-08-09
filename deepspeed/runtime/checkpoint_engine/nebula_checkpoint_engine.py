1b50ad853022407b591273df5221516135ba6f98
Qinghuan Rao <qinghuanrao@microsoft.com>
2022-08-09 16:26:17 +0800

    refine

--- deepspeed/runtime/checkpoint_engine/nebula_checkpoint_engine.py
+++ deepspeed/runtime/checkpoint_engine/nebula_checkpoint_engine.py
@@ -42,9 +42,9 @@ class NebulaCheckpointEngine(CheckpointEngine):
 
         tag = _get_tag_from_path(path)
         partititon_name = os.path.basename(path)
-        logger.info(f"[Nebula] Saving {partititon_name} under tag {tag}...")
+        logger.info(f"[Nebula] Saving {partititon_name} under tag{tag}...")
         self.checkpoint.save(partititon_name, state_dict)
-        logger.info(f"[Nebula] Saved {partititon_name} under tag {tag}.")
+        logger.info(f"[Nebula] Saved {partititon_name} under tag{tag}.")
         return None
 
     def load(self, path: str, map_location=None):
@@ -61,24 +61,21 @@ class NebulaCheckpointEngine(CheckpointEngine):
 
         partititon_name = os.path.basename(path)
         logger.info(
-            f"[Nebula] Loading {path} under tag {tag} from nebula path {self.nebula_load_path}..."
-        )
+            f"[Nebula] Loading {path} under tag{tag} from {self.nebula_load_path}...")
 
         checkpoint = None
-        if tag in (None, 'latest', 'latest_universal'):
+        if tag is None:
             checkpoint = torch_nebula.get_latest_checkpoint(
                 persist_path=self.nebula_load_path)
+            if checkpoint is None or (checkpoint is not None and checkpoint.tag == ''):
+                logger.warning(f"Unable to find latest valid checkpoint from Nebula!")
+                return None
         else:
             checkpoint = torch_nebula.get_checkpoint(tag=tag,
                                                      persist_path=self.nebula_load_path)
-
-        if checkpoint is None or (checkpoint is not None and checkpoint.tag == ''):
-            logger.warning(f"Unable to find latest valid checkpoint from Nebula!")
-            return None
-
         partition = checkpoint.load(partititon_name, map_location=map_location)
         logger.info(
-            f"[Nebula] Loaded {path} under tag {tag} from {self.nebula_load_path}.")
+            f"[Nebula] Loaded {path} under tag{tag} from {self.nebula_load_path}.")
         return partition
 
     def commit(self, tag):
